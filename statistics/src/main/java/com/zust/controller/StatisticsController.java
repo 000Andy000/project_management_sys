5ec1bf1cc18f4dc50944e619e9075eda06527242
Andy <lad949739940@163.com>
2024-01-04 22:41:18 +0800

    🛠️功能完善：DTO PO互转工具, 初始化统计模块

---
+++ statistics/src/main/java/com/zust/controller/StatisticsController.java
@@ -0,0 +1,13 @@
+package com.zust.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Andy
+ * @date 2024-1-4 004 22:40
+ */
+@RestController
+@RequestMapping("/statistics")
+public class StatisticsController {
+}
