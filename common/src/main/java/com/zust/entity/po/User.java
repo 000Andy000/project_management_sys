420d9b1c35b8d36e55a1082a7c88a44fce731f83
Andy <lad949739940@163.com>
2024-01-04 11:57:04 +0800

    📦初始化：项目模块初始化

--- common/src/main/java/com/zust/entity/po/User.java
+++ common/src/main/java/com/zust/entity/po/User.java
@@ -6,6 +6,7 @@ import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
 
+import java.io.Serializable;
 import java.util.Date;
 
  /**
@@ -14,7 +15,7 @@ import java.util.Date;
  */
 @Data
 @TableName("user")
-public class User implements Serializable{
+public class User implements Serializable {
    
     /** 用户id */
     @TableId
